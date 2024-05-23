7d97d21d8f6ce1d45df93c262291f536eb680b7b
Japhet Tegtmeyer <japhteg@gmail.com>
2024-05-23 09:09:39 -0400

    Initial Commit

---
+++ Objec-Bingo/Objec_BingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Objec_BingoApp.swift
+//  Objec-Bingo
+//
+//  Created by Japhet Tegtmeyer on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Objec_BingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
